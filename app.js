cf26211c530c46690e55a7229fa5dbab3de2eb0a
Snega <snega14102001@gmail.com>
2026-02-27 11:10:30 +0530

    added app.js

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 // Import and use the exported function
-import {add} from "./math.js";
-console.log(add(5, 3));
-
-
-//Require and use a CommonJS module
-const multiply = require("./math");
-
-console.log(multiply(4, 5));
+import {multiply} from "./math.js";
+console.log(multiply(5, 3));
 
 
